f1128fa74680aab0ba8aaa924c414b8cba9ee26b
platinumbum <rsdniki@gmail.com>
2021-01-05 14:46:25 +0200

    Add files via upload

---
+++ Programming Basics with C#/First Steps In Coding/08. Pet Shop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08._Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dogs = double.Parse(Console.ReadLine());
+            int animals = int.Parse(Console.ReadLine());
+
+            double dogFood = dogs * 2.5;
+            int animalFood = animals * 4;
+
+
+            Console.WriteLine(dogFood + animalFood + " lv.");
+        }
+    }
+}
---
+++ Programming Basics with C#/First Steps In Coding/09. Yard Greening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _09._Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double yardSize = double.Parse(Console.ReadLine());
+            double price = yardSize * 7.61;
+            double discount = 0.18 * price;
+            double finalPrice = price - discount;
+
+            Console.WriteLine("The final price is " + finalPrice + " lv.");
+            Console.WriteLine("The discount is " + discount + " lv.");
+        }
+    }
+}
