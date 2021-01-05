7b34d35af8ffadf27be310ff9b0c66a2a122d179
platinumbum <rsdniki@gmail.com>
2021-01-05 14:31:56 +0200

    Add files via upload

---
+++ Programming Basics with C#/First Steps In Coding/03. Square Area/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Square_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
