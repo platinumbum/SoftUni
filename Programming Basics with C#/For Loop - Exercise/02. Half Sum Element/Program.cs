d4a64a6374db0a791de53c165006bf6cc13782e2
platinumbum <rsdniki@gmail.com>
2021-01-10 14:55:51 +0200

    Add files via upload

---
+++ Programming Basics with C#/For Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNum = 0;
+
+            for (int i = 0; i < n; i++)
+
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+                if (number > maxNum)
+                {
+                    maxNum = number;
+                }
+
+
+            }
+            if (sum - maxNum == maxNum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(maxNum - (sum - maxNum))}");
+            }
+        }
+    }
+}
---
+++ Programming Basics with C#/For Loop - Exercise/06. Salary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _06._Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Facebook = 150;
+            const int Instagram = 100;
+            const int Reddit = 50;
+
+            int openedTabs = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < openedTabs; i++)
+            {
+
+                string website = Console.ReadLine();
+
+                if (website == "Facebook")
+                {
+                    salary -= Facebook;
+                }
+                else if (website == "Instagram")
+                {
+                    salary -= Instagram;
+                }
+                else if (website == "Reddit")
+                {
+                    salary -= Reddit;
+                }
+
+
+            }
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+                
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
