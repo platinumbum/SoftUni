6621b58b5816f1d04d2415ebe3b8706f04ef0d7f
platinumbum <rsdniki@gmail.com>
2021-01-10 16:26:04 +0200

    Add files via upload

---
+++ Programming Basics with C#/NestedLoops - Exercise/03. Sum Prime Non Prime/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03._Sum_Prime_Non_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "stop")
+            {
+                int input = int.Parse(command);
+
+                if (input < 0)
+                {
+                    Console.WriteLine("Number is negative");
+                    continue;
+                }
+
+            }
+        }
+    }
+}
