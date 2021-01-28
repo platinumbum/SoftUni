7183b32ffb5ea4622ade2376eb24cabb629e18b5
platinumbum <rsdniki@gmail.com>
2021-01-28 12:51:09 +0200

    Add files via upload

---
+++ C#Advanced/Sets and Dictionaries Advanced - Lab/06. Parking Lot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            HashSet<string> cars = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] command = input.Split(", ");
+                string inOrOut = command[0];
+                string car = command[1];
+
+                if (inOrOut == "IN")
+                {
+                    cars.Add(car);
+                }
+                else
+                {
+                    cars.Remove(car);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (cars.Count <= 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
---
+++ C#Advanced/Sets and Dictionaries Advanced - Lab/07. SoftUni Party/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> regGuests = new HashSet<string>();
+            HashSet<string> vipGuests = new HashSet<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "PARTY")
+                {
+                    break;
+                }
+
+                if (char.IsDigit(input[0]))
+                {
+                    vipGuests.Add(input);
+                }
+                else
+                {
+                    regGuests.Add(input);
+                }
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (char.IsDigit(input[0]))
+                {
+                    vipGuests.Remove(input);
+                }
+                else
+                {
+                    regGuests.Remove(input);
+                }
+            }
+
+            Console.WriteLine(vipGuests.Count + regGuests.Count);
+
+            foreach (var guest in vipGuests)
+            {
+                Console.WriteLine(guest);
+            }
+            foreach (var guest in regGuests)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
