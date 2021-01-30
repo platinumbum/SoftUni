ca9b8215c7c6a68483132856c6d92c96396f59c2
platinumbum <rsdniki@gmail.com>
2021-01-30 15:31:13 +0200

    Add files via upload

---
+++ C#Advanced/02. Sets Of Elements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_Of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> numbersInN = new HashSet<int>();
+            HashSet<int> numbersInM = new HashSet<int>();
+            HashSet<int> finalNumbers = new HashSet<int>();
+
+            int n = input[0];
+            int m = input[1];
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbersInN.Add(number);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbersInM.Add(number);
+            }
+
+            foreach (var number in numbersInN)
+            {
+                if (numbersInM.Contains(number))
+                {
+                    finalNumbers.Add(number);
+                }
+            }
+            Console.WriteLine(string.Join(" ",finalNumbers));
+            
+
+        }
+    }
+}
---
+++ C#Advanced/04. Even Times/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<double> evenNumber = new HashSet<double>();
+            double theNumber = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (evenNumber.Contains(number))
+                {
+                    theNumber = number;
+                }
+                evenNumber.Add(number);
+
+            }
+            Console.WriteLine(theNumber);
+        }
+    }
+}
