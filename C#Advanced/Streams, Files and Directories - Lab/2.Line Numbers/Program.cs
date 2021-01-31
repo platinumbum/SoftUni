221a2e4adcb92713b3a81add537449a4d812c82a
platinumbum <rsdniki@gmail.com>
2021-01-31 20:40:27 +0200

    Add files via upload

---
+++ C#Advanced/Streams, Files and Directories - Lab/2.Line Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("../../../input.txt");
+
+            using (reader)
+            {
+                int counter = 1;
+                string line = reader.ReadLine();
+
+                using(StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C#Advanced/Streams, Files and Directories - Lab/3. Word Count/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _3._Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("../../../words.txt");
+
+            using (reader)
+            {
+                string[] words = reader.ReadToEnd().ToString().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => x.ToLower()).ToArray();
+                Dictionary<string, int> counter = new Dictionary<string, int>();
+                for (int i = 0; i < words.Length; i++)
+                {
+                    counter.Add(words[i], 0);
+                }
+
+                using(StreamReader inputReader = new StreamReader("../../../input.txt"))
+                {
+                    string[] searchedWords = inputReader.ReadToEnd().Split(new char[] { ' ', ',', '-', '.' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.ToLower()).ToArray();
+                    for (int i = 0; i < searchedWords.Length; i++)
+                    {
+                        if (counter.ContainsKey(searchedWords[i]))
+                        {
+                            counter[searchedWords[i]]++;
+                        }
+                    }
+                    using (StreamWriter wrirer = new StreamWriter("../../../output.txt"))
+                    {
+                        counter = counter.OrderByDescending(x => x.Value).ToDictionary(a => a.Key, b => b.Value);
+                        foreach (var item in counter)
+                        {
+                            wrirer.WriteLine($"{item.Key} - {item.Value}");
+                        }
+                    }
+
+                }
+            }
+
+
+        }
+    }
+}
