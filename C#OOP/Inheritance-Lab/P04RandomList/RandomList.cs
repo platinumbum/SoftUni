e90e96b15376e42cd1782a70e498cdaaa9280476
platinumbum <rsdniki@gmail.com>
2021-02-26 09:30:17 +0200

    Add files via upload

---
+++ C#OOP/Inheritance-Lab/P04RandomList/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rnd;
+
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+        public string RemoveRandomElement()
+        {
+            return this[rnd.Next(0, this.Count)];
+        }
+    }
+}
