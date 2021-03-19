c6d91be5abb6728bf3b036a50251d60f44e4ab98
platinumbum <rsdniki@gmail.com>
2021-03-19 22:22:28 +0200

    Add files via upload

---
+++ C#OOP/Polymorphism-Lab/P03Shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return this.height * this.width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return this.height + this.width + this.height + this.width;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
