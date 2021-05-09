eab285dba70ac5575d4892953b87c6d2ff6f9d99
platinumbum <rsdniki@gmail.com>
2021-05-09 15:48:18 +0300

    Add files via upload

---
+++ C#Fundamentals/DataTypesAndVariables-Exercise/P11Snowballs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P11Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int bestQuality = 0;
+            int bestValue = 0;
+            int bestSnow = 0;
+            int bestTime = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+
+                int snowballValue = (int)Math.Pow(snowballSnow / snowballTime, (int)snowballQuality);
+                if (snowballValue > bestValue)
+                {
+                    bestQuality = snowballQuality;
+                    bestValue = snowballValue;
+                    bestTime = snowballTime;
+                    bestSnow = snowballSnow;
+                }
+            }
+            Console.WriteLine($"{bestSnow} : {bestTime} = {bestValue} ({bestQuality})");
+        }
+    }
+}
