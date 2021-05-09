203a66bc5f36c511324c0fb16959f18de2346674
platinumbum <rsdniki@gmail.com>
2021-05-09 21:29:34 +0300

    Add files via upload

---
+++ C#Fundamentals/Arrays-Lab/P02PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace P02PrintNumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                arr[i] = currentNum;
+            }
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                int tmp = arr[i];
+                arr[i] = arr[arr.Length - i - 1];
+                arr[arr.Length - i - 1] = tmp;
+            }
+            Console.WriteLine(string.Join(" ",arr));
+        }
+    }
+}
---
+++ C#Fundamentals/Arrays-Lab/P03RoundingNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace P03RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine($"{(decimal)nums[i]} => {Math.Round((decimal)nums[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
