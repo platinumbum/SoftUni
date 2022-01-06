a494a20403a7a0c6475c324ad04334d4b608ff5c
platinumbum <rsdniki@gmail.com>
2022-01-06 13:49:27 +0200

    Add files via upload

---
+++ C#Fundamentals/Lists-Lab/P02GaussTrick/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P02GaussTrick // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            
+
+            int halfIndex = numbers.Count / 2;
+
+            for (int i = 0; i < halfIndex; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.Remove(numbers[numbers.Count - 1]);
+            }
+            Console.WriteLine(String.Join(" ", numbers));   
+        }
+    }
+}
