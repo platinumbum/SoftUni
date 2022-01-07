c3e5b7b1d16683c4a5fb92cf634c2719aa1eaf42
platinumbum <rsdniki@gmail.com>
2022-01-07 14:59:44 +0200

    Add files via upload

---
+++ C#Fundamentals/Lists-Lab/P03MergingLists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace P03MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> final = new List<int>();
+
+            if (firstNumbers.Count > secondNumbers.Count)
+            {
+                for (int i = 0; i < firstNumbers.Count; i++)
+                {
+                    final.Add(firstNumbers[i]);
+                    if (secondNumbers.Count > i)
+                    {
+                        final.Add(secondNumbers[i]);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < secondNumbers.Count; i++)
+                {
+                    if (firstNumbers.Count > i)
+                    {
+                        final.Add(firstNumbers[i]);
+                    }
+                    final.Add(secondNumbers[i]);
+                }
+            }
+
+
+            Console.WriteLine(String.Join(" ", final));
+        }
+    }
+}
