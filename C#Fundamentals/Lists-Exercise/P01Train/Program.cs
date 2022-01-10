14835d608199404908920aff3e509478a36bc141
platinumbum <rsdniki@gmail.com>
2022-01-10 14:01:54 +0200

    Add files via upload

---
+++ C#Fundamentals/Lists-Exercise/P01Train/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split();
+                if (tokens[0] == "Add")
+                {
+                    wagons.Add(int.Parse(tokens[1]));
+                }
+                else
+                {
+                    int passengers = int.Parse(tokens[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (passengers + wagons[i] <= maxCapacity)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(" ", wagons));
+        }
+    }
+}
