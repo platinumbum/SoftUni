cbfa4f8ea600507f84498f3c528a499001c6de2a
platinumbum <rsdniki@gmail.com>
2022-01-15 16:37:20 +0200

    Add files via upload

---
+++ C#Fundamentals/Lists-Exercise/P03HouseParty/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            List<string> guests = new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                string person = command[0];
+                if (command.Length > 3)
+                {
+                    if (guests.Contains(person))
+                    {
+                        guests.Remove(person);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{person} is not in the list!");
+                    }
+                }
+                else
+                {
+                    if (guests.Contains(person))
+                    {
+                        Console.WriteLine($"{person} is already in the list!");
+                    }
+                    else
+                    {
+                        guests.Add(person);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, guests));
+        }
+    }
+}
---
+++ C#Fundamentals/Lists-Exercise/P04ListOperations/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04ListOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] tokens = input.Split();
+                string cmdArg = tokens[0];
+                if (cmdArg == "Add")
+                {
+                    int number = int.Parse(tokens[1]);
+                    numbers.Add(number);
+                }
+                else if (cmdArg == "Insert")
+                {
+
+                    int number = int.Parse(tokens[1]);
+                    int index = int.Parse(tokens[2]);
+                    if (index > numbers.Count || index < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    else
+                    {
+                        numbers.Insert(index, number);
+                    }
+                    
+
+                }
+                else if (cmdArg == "Remove")
+                {
+                    int number = int.Parse(tokens[1]);
+                    if (number > numbers.Count || number < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                    }
+                    else
+                    {
+                        numbers.RemoveAt(number);
+                    }
+                    
+                }
+                else if (cmdArg == "Shift")
+                {
+                    int count = int.Parse(tokens[2]);
+                    if (tokens[1] == "left")
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            int firstElement = numbers[0];
+                            for (int j = 0; j < numbers.Count - 1; j++)
+                            {
+                                numbers[j] = numbers[j + 1];
+                            }
+
+                            numbers[numbers.Count - 1] = firstElement;
+                        }
+                    }
+                    else  
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            int lastElement = numbers[numbers.Count - 1];
+                            for (int j = numbers.Count - 1;j > 0; j--)
+                            {
+                                numbers[j] = numbers[j - 1];
+                            }
+                            numbers[0] = lastElement;
+                        }
+                    }
+                }
+                
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
