e841d9b6e556ef67afe8bc7d0720bb66650a117a
platinumbum <rsdniki@gmail.com>
2022-02-05 13:11:36 +0200

    Add files via upload

---
+++ C#Fundamentals/Lists-Exercise/P06CardsGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> playerOne = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> playerTwo = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (playerOne.Count != 0 && playerTwo.Count != 0)
+            {
+
+                int firstCard = playerOne[0];
+                int secondCard = playerTwo[0];
+
+                if (firstCard > secondCard)
+                {
+                    playerOne.Add(firstCard);
+                    playerOne.Add(secondCard);
+
+                    playerOne.Remove(firstCard);
+                    playerTwo.Remove(secondCard);
+                }
+                else if (firstCard < secondCard)
+                {
+                    playerTwo.Add(secondCard);
+                    playerTwo.Add(firstCard);
+
+                    playerTwo.Remove(secondCard);
+                    playerOne.Remove(firstCard);
+                }
+                else if (firstCard == secondCard)
+                {
+                    playerOne.Remove(firstCard);
+                    playerTwo.Remove(secondCard);
+                }
+            }
+
+            if (playerOne.Count > playerTwo.Count)
+            {
+                Console.WriteLine($"First player wins! Sum: {playerOne.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Second player wins! Sum: {playerTwo.Sum()}");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/Lists-Exercise/P07AppendArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P07AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split("|").ToList();
+
+            List<string> result = new List<string>();
+
+            items.Reverse();
+
+            foreach (var currentItem in items)
+            {
+                string[] numbers = currentItem.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                foreach (var numbersToAdd in numbers)
+                {
+                    result.Add(numbersToAdd);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
